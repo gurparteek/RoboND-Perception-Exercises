84ec399811cc504e3868e55e35bdd0b93d13fa2d
gurparteek <gurparteek62@gmail.com>
2017-08-24 14:12:34 -0400

    Train SVM and save model 'model1.sav' for the World 1 of the Robotics Perception Project.

--- Exercise-3/sensor_stick/scripts/capture_features.py
+++ Exercise-3/sensor_stick/scripts/capture_features.py
@@ -24,13 +24,9 @@ if __name__ == '__main__':
     rospy.init_node('capture_node')
 
     models = [\
-       'beer',
-       'bowl',
-       'create',
-       'disk_part',
-       'hammer',
-       'plastic_cup',
-       'soda_can']
+       'biscuits',
+       'soap',
+       'soap2']
 
     # Disable gravity and delete the ground plane
     initial_setup()
